6c8ff80169e8b414ee523c68b761908a5c212539
Aviv Weissman <Avivw08@gmail.com>
2020-03-29 18:41:15 +0300

    backup
    backing up the first unity project

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseManager : MonoBehaviour
+{
+    //know what objects are clickable
+    public LayerMask clickableLayer;
+
+    //swap cursors per object 
+    public Texture2D pointer; // Normal pointer
+    public Texture2D target; // Cursor for clickable objects like the world
+    public Texture2D doorway; // Cursor for doorway
+    public Texture2D combat; // Cursor for combat
+
+    public EventVector3 OnClickEnviorment;
+
+  
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            bool item = false;
+
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+
+            else if (hit.collider.gameObject.tag == "Item")
+            {
+                Cursor.SetCursor(combat, new Vector2(16, 16), CursorMode.Auto);
+                item = true;
+            }
+
+
+            else
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (door)
+                {
+                    Transform doorway = hit.collider.gameObject.transform;
+
+                    OnClickEnviorment.Invoke(doorway.position);
+                    Debug.Log("DOOR");
+                }
+                else if (item)
+                {
+                    Transform itemPos = hit.collider.gameObject.transform;
+
+                    OnClickEnviorment.Invoke(itemPos.position);
+                    Debug.Log("ITEM");
+                }
+                else
+                {
+                    OnClickEnviorment.Invoke(hit.point);
+                }
+            }
+        }
+
+        else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour
+{
+    public float patrolTime = 10f;
+    public float aggroRange = 10f;
+    public Transform[] waypoints;
+
+    private int index;
+    private float speed, agentSpeed;
+    private Transform Player;
+
+    private Animator anim;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        if (agent != null){ agentSpeed = agent.speed;}
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        index = Random.Range(0, waypoints.Length);
+
+        InvokeRepeating("Tick", 0, 0.5f);
+
+        if (waypoints.Length> 0)
+        {
+            InvokeRepeating("Patrol", 0, patrolTime);
+        }
+
+    }
+
+    void Patrol()
+    {
+        index = index == waypoints.Length - 1 ?  0 : index + 1;
+    }
+
+
+    void Tick()
+    {
+        agent.destination = waypoints[index].position;
+        agent.speed = agentSpeed / 2;
+
+        if (Player != null && Vector3.Distance(transform.position, Player.position) < aggroRange)
+        {
+            anim.SetFloat("Chase", 0.8f);
+            agent.destination = Player.position;
+            agent.speed = agentSpeed;
+            if (Player != null && Vector3.Distance(transform.position, Player.position) <= 1.55)
+            {
+                anim.SetFloat("Chase", 0f);
+            }
+        }
+    }
+    private void Update()
+    {
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+    
+    }
+
+}
